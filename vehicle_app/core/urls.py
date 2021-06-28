f3ddb6bf14d81978a936d438cc74f8e170e5d9c7
José Vitor Santos Silva <jose.silva@dcomp.ufs.br>
2021-06-28 17:40:57 -0300

    Inicio criação tela veículos

--- vehicle_app/core/urls.py
+++ vehicle_app/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from core import views
+from . import views
 
 urlpatterns = [
     path('', views.home),
--- vehicle_app/vehicle_app/settings.py
+++ vehicle_app/vehicle_app/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -123,4 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'core', 'static'),
+)
